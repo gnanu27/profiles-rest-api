298cf735690c7560db3ed6d6cb3e8999f235b6e5
gnanu27 <gnanavel.py2k@gmail.com>
2020-02-22 17:16:37 +0530

    login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets, filters
 from rest_framework.authentication import TokenAuthentication
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from . import serializers
 from profiles_api import models
 from profiles_api import permissions
@@ -114,3 +115,10 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """
+        Handle Creating User authentication tokens
+    """
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
